eaf6e836f21cc3c980566691abf43863f0dd7f26
Pavlo Glushko <pavloglushko@gmail.com>
2019-10-14 17:29:46 +0200

    correct test instances

--- tests/unit_tests.py
+++ tests/unit_tests.py
@@ -11,11 +11,9 @@ Model.setDebug(True)
 class TestConverter(TestCase):
 
     def test_run(self):
-        filename = 'mps_instance.mps'
+        filename = 'Dakota_det.mps'
         format = 'mpl'
-        f = Path(filename)
-        f.write_text('text')
-        converter = Converter(filename, format)
+        converter = Converter(filename, format, 'Dakota_det_converted')
         self.assertTrue(converter.run())
 
     def test_run_no_file(self):
@@ -27,20 +25,16 @@ class TestConverter(TestCase):
         self.assertEqual(str(e.exception), Messages.MSG_INSTANCE_FILE_NOT_FOUND)
 
     def test_run_not_supported_in_format(self):
-        filename = 'instance.trk'
+        filename = 'Dakota_det.trk'
         format = 'mpl'
-        f = Path(filename)
-        f.write_text('text')
         converter = Converter(filename, format)
         with self.assertRaises(ValueError) as e:
             converter.run()
         self.assertEqual(str(e.exception), Messages.MSG_INPUT_FORMAT_NOT_SUPPORTED)
 
     def test_run_not_supported_out_format(self):
-        filename = 'instance_1.mps'
+        filename = 'Dakota_det.mps'
         format = 'trk'
-        f = Path(filename)
-        f.write_text('text')
         converter = Converter(filename, format)
         with self.assertRaises(ValueError) as e:
             converter.run()
@@ -48,7 +42,7 @@ class TestConverter(TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        temp_files = ['mps_instance.mps', 'instance.trk', 'instance_1.mps']
+        temp_files = []
         for filename in temp_files:
             f = Path(filename)
             if f.is_file():
@@ -59,30 +53,20 @@ class TestModel(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.temp_files = ['mps_instance.mps', 'instance.trk', 'mpl_instance.mpl', 'lp_instance.lp', 'mps_instance_2.mps']
         cls.initial_argv = sys.argv
-        for file in cls.temp_files:
-            f = Path(file)
-            f.write_text('text')
 
-    def test__init_mpl(self):
+    def test__init(self):
         filename = 'Dakota_det'
-        format = 'mpl'
-        model = Model(Path(f'{filename}.{format}'))
-        self.assertEqual(model.file.stem, filename)
+        for format in ['lp', 'mpl', 'mps']:
+            model = Model(Path(f'{filename}.{format}'))
+            self.assertEqual(abs(model.solve()), 4169.0)
 
-    def test__init_mpl_wrong(self):
+    def test__init_wrong(self):
         filename = 'Dakota_det_wrong'
         format = 'mpl'
         with self.assertRaises(ModelResultException) as e:
             Model(Path(f'{filename}.{format}'))
         self.assertEqual(str(e.exception)[:88], "The Model.ReadModel(filename='Dakota_det_wrong.mpl') method returned result='ParserError")
-
-    def test__init_lp(self):
-        filename = 'Dakota_det'
-        format = 'lp'
-        model = Model(Path(f'{filename}.{format}'))
-        self.assertEqual(model.file.stem, filename)
 
     def test__init_not_existing_file(self):
         filename = 'mps_instance_na'
@@ -92,36 +76,49 @@ class TestModel(TestCase):
         self.assertEqual(str(e.exception), Messages.MSG_INSTANCE_FILE_NOT_FOUND)
 
     def test__init_not_supported_in_file(self):
-        filename = 'instance'
+        filename = 'Dakota_det'
         format = 'trk'
         with self.assertRaises(ValueError) as e:
             Model(Path(f'{filename}.{format}'))
         self.assertEqual(str(e.exception), Messages.MSG_INPUT_FORMAT_NOT_SUPPORTED)
 
     def test__save(self):
-        filename = 'mps_instance'
-        format = 'mps'
-        model = Model(Path(f'{filename}.{format}'))
-        self.assertTrue(model.save())
-
-    def test__save_another_file_and_format(self):
-        filename = 'mps_instance'
-        format = 'mps'
-        model = Model(Path(f'{filename}.{format}'))
-        self.assertTrue(model.save('lp', 'new_instances'))
+        filename = 'Dakota_det'
+        in_formats = ['mpl', 'mps', 'lp']
+        out_formats = ['mpl', 'mps', 'lp']
+        for in_format in in_formats:
+            model = Model(Path(f'{filename}.{in_format}'))
+            for out_format in out_formats:
+                print(f'Testing In format: {in_format} Out format: {out_format}')
+                model.save(out_format, f'{filename}_converted')
+                model_new = Model(Path(f'{filename}_converted.{out_format}'))
+                self.assertEqual(abs(model_new.solve()), 4169.0)
 
     def test_save_not_supported_out_format(self):
-        filename = 'mps_instance'
-        format = 'mps'
+        filename = 'Dakota_det'
+        format = 'mpl'
         out_format = 'trk'
         model = Model(Path(f'{filename}.{format}'))
         with self.assertRaises(ValueError) as e:
             model.save(format=out_format)
         self.assertEqual(str(e.exception), Messages.MSG_OUT_FORMAT_NOT_SUPPORTED)
 
+    def test__save_stochastic(self):
+        filename = 'SNDP_stochastic_MIP'
+        in_format = 'mpl' # only mpl works for for stochastic, MPS does not work
+        out_format = 'mps' # only mps out works
+        model = Model(Path(f'{filename}.{in_format}'))
+        print(f'Testing In format: {in_format} Out format: {out_format}')
+        model.save(out_format, f'{filename}_converted')
+        model_new = Model(Path(f'{filename}_converted.{out_format}'))
+        self.assertEqual(model_new.solve(), -15250.0)
+
+    def test__save_not_supported_out_stoch_format(self):
+        self.assertTrue(False)
+
     @classmethod
     def tearDownClass(cls):
-        for file in cls.temp_files + ['new_instance.lp']:
+        for file in ['new_instance.lp', 'Dakota_det_converted.mpl', 'Dakota_det_converted.mps', 'Dakota_det_converted.lp', 'Dakota_det_converted_after_parse_file().mpl', 'Dakota_det_after_parse_file().mpl']:
             f = Path(file)
             if f.is_file():
                 f.unlink()
@@ -131,7 +128,7 @@ class Test_command_line(TestCase):
 
     @classmethod
     def setUpClass(cls):
-        cls.temp_files = ['mps_instance.mps', 'instance.trk', 'mpl_instance.mpl', 'lp_instance.lp', 'mps_instance_2.mps']
+        cls.temp_files = []
         cls.initial_argv = sys.argv
         for file in cls.temp_files:
             f = Path(file)
@@ -139,13 +136,13 @@ class Test_command_line(TestCase):
 
     def test_parse_args(self):
         filename = 'mps_instance.mps'
-        fileformat = 'mpl'
+        fileformat = 'sim'
         parsed = parse_args(['--file', filename, '--out_format', fileformat])
         self.assertEqual(parsed.files, [filename])
         self.assertEqual(parsed.out_format, fileformat)
 
     def test_parse_args_no_filename(self):
-        fileformat = 'mpl'
+        fileformat = 'sim'
         parsed = parse_args(['--out_format', fileformat])
         self.assertEqual(parsed.files, [])
         self.assertEqual(parsed.out_format, fileformat)
@@ -163,10 +160,10 @@ class Test_command_line(TestCase):
 
     @patch('builtins.input', side_effect=['y'])
     def test_command_line(self, input):
-        filename = 'mps_instance.mps'
-        format = 'mpl'
+        filename = 'Dakota_det.mpl'
+        format = 'sim'
         sys.argv = sys.argv + ['--file', filename, '--out_format', format]
-        self.assertTrue(command_line())
+        self.assertEqual(command_line(), Messages.MSG_FILE_CONVERTED)
 
     @patch('builtins.input', side_effect=['y'])
     def test_command_line_file_not_exists(self, input):
@@ -178,27 +175,27 @@ class Test_command_line(TestCase):
     @patch('builtins.input', side_effect=['n', 0, 'y'])
     def test_command_line_file_not_exists_ask_againe_answer_file(self, input):
         filename = 'instance_na.mps'
-        format = 'mpl'
+        format = 'sim'
         sys.argv = sys.argv + ['--file', filename, '--out_format', format]
-        self.assertTrue(command_line())
+        self.assertTrue(str(command_line()), Messages.MSG_INSTANCE_FILE_NOT_FOUND)
 
     @patch('builtins.input', side_effect=[100, 0, 'y'])
     def test_command_line_no_file_ask_againe_answer_wrong_index(self, input):
-        format = 'mpl'
+        format = 'sim'
         sys.argv = sys.argv + ['--out_format', format]
         self.assertTrue(command_line())
 
     @patch('builtins.input', side_effect=[4, 'y'])
     def test_command_line_no_file_ask_againe_answer_extension(self, input):
-        format = 'mpl'
+        format = 'sim'
         sys.argv = sys.argv + ['--out_format', format]
         self.assertTrue(4 >= len(self.temp_files)-2, msg='Second mocked value should choose extension.')
         self.assertTrue(command_line())
 
     @patch('builtins.input', side_effect=['y'])
     def test_command_line_not_supported_in_format(self, input):
-        filename = 'instance.trk'
-        format = 'mpl'
+        filename = 'Dakota_det.trk'
+        format = 'sim'
         sys.argv = sys.argv + ['--file', filename, '--out_format', format]
         self.assertEqual(str(command_line()), Messages.MSG_INPUT_FORMAT_NOT_SUPPORTED)
 
@@ -208,7 +205,7 @@ class Test_command_line(TestCase):
 
     @classmethod
     def tearDownClass(cls):
-        for file in cls.temp_files:
+        for file in cls.temp_files + ['Dakota_det.sim', 'Dakota_det_after_parse_file().mpl']:
             f = Path(file)
             if f.is_file():
                 f.unlink()
